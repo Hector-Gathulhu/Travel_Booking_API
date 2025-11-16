c5cde7cccc5b2e8965469a8b5f5de0eba5e8baa4
Hector Gonzalez S <Heco_gs@hotmail.com>
2025-11-16 14:23:12 -0600

    add mapping method ticket response-entity

--- src/main/java/com/cursospring/best_travel/domain/entities/TicketEntity.java
+++ src/main/java/com/cursospring/best_travel/domain/entities/TicketEntity.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "ticket")
@@ -19,8 +20,8 @@ public class TicketEntity {
 
     @Id
     private UUID id;
-    private LocalDate departureDate;
-    private LocalDate arrivalDate;
+    private LocalDateTime departureDate;
+    private LocalDateTime arrivalDate;
     private LocalDate purchaseDate;
     private BigDecimal price;
 
--- src/main/java/com/cursospring/best_travel/infraestructure/services/TicketService.java
+++ src/main/java/com/cursospring/best_travel/infraestructure/services/TicketService.java
@@ -1,12 +1,35 @@
 package com.cursospring.best_travel.infraestructure.services;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import com.cursospring.best_travel.api.models.request.TicketRequest;
+import com.cursospring.best_travel.api.models.responses.FlyResponse;
 import com.cursospring.best_travel.api.models.responses.TicketResponse;
+import com.cursospring.best_travel.domain.entities.FlyEntity;
+import com.cursospring.best_travel.domain.entities.TicketEntity;
+import com.cursospring.best_travel.domain.repositories.CustomerRepository;
+import com.cursospring.best_travel.domain.repositories.FlyRepository;
+import com.cursospring.best_travel.domain.repositories.TicketRepository;
 import com.cursospring.best_travel.infraestructure.abstract_services.ITicketService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@Transactional
+@Service
+@Slf4j
+@AllArgsConstructor
 public class TicketService implements ITicketService {
+
+    private final FlyRepository flyRepository;
+    private final CustomerRepository customerRepository;
+    private final TicketRepository ticketRepository;
+
+
+
     @Override
     public TicketResponse create(TicketRequest request) {
         return null;
@@ -25,5 +48,14 @@ public class TicketService implements ITicketService {
     @Override
     public void delete(UUID uuid) {
 
+    }
+
+    private TicketResponse entityToResponse(TicketEntity entity){
+        var response = new TicketResponse();
+        BeanUtils.copyProperties(entity,response);
+        var flyResponse = new FlyResponse();
+        BeanUtils.copyProperties(entity.getFly(), flyResponse);
+        response.setFly(flyResponse);
+        return response;
     }
 }
