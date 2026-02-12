163e023299e1b65451b4dbe1d6f0f2f4feec16cf
Hector Gonzalez S <Heco_gs@hotmail.com>
2026-02-11 18:23:00 -0600

    create (Reservation-Hotel) DTOs

---
+++ src/main/java/com/cursospring/best_travel/api/models/request/ReservationRequest.java
@@ -0,0 +1,17 @@
+package com.cursospring.best_travel.api.models.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ReservationRequest {
+
+    private String idClient;
+    private Long idHotel;
+}
--- src/main/java/com/cursospring/best_travel/api/models/request/TicketRequest.java
+++ src/main/java/com/cursospring/best_travel/api/models/request/TicketRequest.java
@@ -12,6 +12,9 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Data
 @Builder
+//Request DTOs are made to request some information to the user and execute any of the CRUD requests.
+//In this example we request to the user to give his ID and a ID fly in order to CRUD a ticket
+//Example, we'll use TicketResponse in a Create request. Requesting just below two parameters and avoid any other unnecessary parameter
 public class TicketRequest implements Serializable {
 
     private String idClient;    
