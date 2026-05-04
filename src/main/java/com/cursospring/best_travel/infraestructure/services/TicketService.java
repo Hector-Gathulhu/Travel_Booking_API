6a0a4fa939559bd0e6b3158bc59044b3498dd7c9
Hector Gonzalez S <Heco_gs@hotmail.com>
2026-05-04 17:19:17 -0600

    add date format function

--- src/main/java/com/cursospring/best_travel/infraestructure/services/TicketService.java
+++ src/main/java/com/cursospring/best_travel/infraestructure/services/TicketService.java
@@ -8,6 +8,7 @@ import com.cursospring.best_travel.domain.repositories.CustomerRepository;
 import com.cursospring.best_travel.domain.repositories.FlyRepository;
 import com.cursospring.best_travel.domain.repositories.TicketRepository;
 import com.cursospring.best_travel.infraestructure.abstract_services.ITicketService;
+import com.cursospring.best_travel.util.BestTravelUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -42,8 +43,8 @@ public class TicketService implements ITicketService {
                 .customer(customer)
                 .price(fly.getPrice().add(fly.getPrice().multiply(charger_price_percentage)))
                 .purchaseDate(LocalDate.now())
-                .arrivalDate(LocalDateTime.now())
-                .departureDate(LocalDateTime.now())
+                .arrivalDate(BestTravelUtil.getRandomLatter())
+                .departureDate(BestTravelUtil.getRandomSoon())
                 .build();
 
         var ticketPersisted = this.ticketRepository.save(ticketToPersist);
@@ -67,8 +68,8 @@ public class TicketService implements ITicketService {
 
         ticketToUpdate.setFly(fly);
         ticketToUpdate.setPrice(fly.getPrice().add(fly.getPrice().multiply(charger_price_percentage)));
-        ticketToUpdate.setDepartureDate(LocalDateTime.now());
-        ticketToUpdate.setArrivalDate(LocalDateTime.now());
+        ticketToUpdate.setDepartureDate(BestTravelUtil.getRandomSoon());
+        ticketToUpdate.setArrivalDate(BestTravelUtil.getRandomLatter());
 
         var ticketUpdated = this.ticketRepository.save(ticketToUpdate);
 
